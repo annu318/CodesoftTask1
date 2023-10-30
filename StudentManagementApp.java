fcfdfeefd5033473a39c5bbfb9765c253c579c8e
Aniket Shelke <115876635+annu318@users.noreply.github.com>
2023-10-30 20:46:38 +0530

    Add files via upload

---
+++ StudentManagementApp.java
@@ -0,0 +1,138 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade;
+    }
+}
+
+class StudentManagementSystem {
+    private ArrayList<Student> students;
+
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(int rollNumber) {
+        students.removeIf(student -> student.getRollNumber() == rollNumber);
+    }
+
+    public Student searchStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllStudents() {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+
+    public ArrayList<Student> getAllStudents() {
+        return students;
+    }
+}
+
+public class StudentManagementApp {
+    public static void main(String[] args) {
+        StudentManagementSystem sms = new StudentManagementSystem();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Student Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. Remove Student");
+            System.out.println("3. Search Student");
+            System.out.println("4. Display All Students");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter Roll Number: ");
+                    int rollNumber = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Enter Grade: ");
+                    String grade = scanner.nextLine();
+                    Student student = new Student(name, rollNumber, grade);
+                    sms.addStudent(student);
+                    break;
+                case 2:
+                    System.out.print("Enter Roll Number to remove: ");
+                    int rollToRemove = scanner.nextInt();
+                    sms.removeStudent(rollToRemove);
+                    break;
+                case 3:
+                    System.out.print("Enter Roll Number to search: ");
+                    int rollToSearch = scanner.nextInt();
+                    Student foundStudent = sms.searchStudent(rollToSearch);
+                    if (foundStudent != null) {
+                        System.out.println("Student found: " + foundStudent);
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+                case 4:
+                    sms.displayAllStudents();
+                    break;
+                case 5:
+                    saveStudentData(sms.getAllStudents());
+                    System.out.println("Exiting the application.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    public static void saveStudentData(ArrayList<Student> students) {
+        try (FileWriter fileWriter = new FileWriter("students.txt");
+             BufferedWriter writer = new BufferedWriter(fileWriter)) {
+            for (Student student : students) {
+                writer.write(student.getName() + "," + student.getRollNumber() + "," + student.getGrade());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
